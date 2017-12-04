fd8b8c82e82be00118357b699e2ad43f793f4d24
G. Mantaos <gmantaos@gmail.com>
2017-12-04 13:36:47 +0200

    fixed a couple of sliders

--- Pleisure/app/js/filters.js
+++ Pleisure/app/js/filters.js
@@ -7,3 +7,19 @@ if (!isTouchDevice()) {
 function isTouchDevice() {
 	return !!('ontouchstart' in window || navigator.msMaxTouchPoints);
 }
+
+
+$(document).ready(function()
+{
+	// Register sliders
+	$('[data-role="rangeslider"] > input').change(function(evt)
+	{
+		// Update the label above the slider
+		$(this).parent()
+			.find('.slider-value')
+			.html($(this).val());
+	});
+
+	// Trigger a change on each for the initial update
+	$('[data-role="rangeslider"] > input').change();
+});
--- Pleisure/app/js/main.js
+++ Pleisure/app/js/main.js
@@ -5,6 +5,5 @@ function update_slider_value() {
 	$(slider_output).html($(slider).val()).append('&nbsp;<span class="fa fa-money"></span>');
 }
 
-$(document).ready(update_slider_value);
 
 $(slider).change(update_slider_value);
