af31b31fed73678be8b36e0023290fd43aa0614e
Nik Gavalas <gavalnick@gmail.com>
2017-12-04 21:19:40 +0200

    login and signup modals

--- Pleisure/app/js/main.js
+++ Pleisure/app/js/main.js
@@ -5,5 +5,19 @@ function update_slider_value() {
 	$(slider_output).html($(slider).val()).append('&nbsp;<span class="fa fa-money"></span>');
 }
 
-
 $(slider).change(update_slider_value);
+
+// Modal Interchange
+function spawnModal(modal) {
+	if (modal === 'signup')
+	{
+		$('#loginModal').modal('hide');
+		$('#signupModal').modal('show');
+	}
+	
+	if (modal === 'login')
+	{
+		$('#signupModal').modal('hide');
+		$('#loginModal').modal('show');
+	}
+}
