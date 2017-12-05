3d0e03b23fbeaa840f40dd37d68e3a8d4b636cc1
Nik Gavalas <gavalnick@gmail.com>
2017-12-05 02:06:32 +0200

    login/signup almost finished, respective modals are functional, (code needs cleanup), bug fixes

--- Pleisure/app/js/main.js
+++ Pleisure/app/js/main.js
@@ -9,15 +9,63 @@ $(slider).change(update_slider_value);
 
 // Modal Interchange
 function spawnModal(modal) {
-	if (modal === 'signup')
-	{
+	if (modal === 'signup') {
 		$('#loginModal').modal('hide');
 		$('#signupModal').modal('show');
 	}
-	
-	if (modal === 'login')
-	{
+
+	if (modal === 'login') {
 		$('#signupModal').modal('hide');
 		$('#loginModal').modal('show');
 	}
 }
+
+// TODO: wrap the following functions properly
+$('#loginForm').submit(function (event) {
+	event.preventDefault();
+
+	var inp = $('#loginForm').serializeArray();
+
+	$.post('/login',
+		{
+			email: inp[0].value,
+			password: inp[1].value
+		},
+		function (resp, status) {
+			$('#loginModal').modal('hide');
+			location.href = "/events";
+		});
+});
+
+function registerLoginSignupHandlers(selector) {
+	$(selector).submit(function(event) {
+		event.preventDefault();
+
+		var inp = $(selector).serializeArray();
+
+		if (inp[5].value != "on") {
+			alert('You need to accept the Terms of Usage in order to sign up.');
+			return;
+		}
+
+		$('.submitAwait').removeClass('fa fa-sign-in').addClass('fa fa-spinner fa-spin fa-fw');
+
+		$.post('/register',
+			{
+				email: inp[0].value,
+				password: inp[3].value,
+				password2: inp[4].value,
+				full_name: inp[1].value + ' ' + inp[2].value,
+				role: $('signupParentPill').hasClass('active') ? 1 : 2
+			},
+			function (resp, status) {
+				// TODO: check status, handle errors (maybe use classes 'alert alert-dismissible alert-success' to display messages)
+				$('#signupModal').modal('hide');
+				location.href = "/events";
+			});
+	});
+}
+
+registerLoginSignupHandlers('#signupParentForm');
+registerLoginSignupHandlers('#signupOrganizerForm');
+
